ca8827921a2300263e1978834bf073d24d2bd95b
Tyler Alfreds <talfreds@my.bcit.ca>
2018-11-15 18:36:14 -0800

    Added code and downloaded middleware module for favicon

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var favicon = require('serve-favicon');
+
+app.use(favicon(__dirname + '/img/favicon.ico'));
+
 const port = process.env.PORT || 8080;
 connect().use(serveStatic(__dirname)).listen(port, function(){
     console.log(`Server running on... ${port}`);
